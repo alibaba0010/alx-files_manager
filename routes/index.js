ae34788754e6e5ddb86c7ef0e268cdb1d8a19b32
alibaba0010 <zakariyahali100@gmail.com>
2024-07-03 14:45:54 +0100

    routes added

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { getStatus, getStats } from '../controllers/AppController';
+import { postNew, getMe } from '../controllers/UsersController';
+import { getConnect, getDisconnect } from '../controllers/AuthController';
+import { postUpload, getShow, getIndex, putPublish, putUnpublish, getFile } from '../controllers/FilesController';
+
+const router = Router();
+
+router.get('/status', getStatus);
+router.get('/stats', getStats);
+router.post('/users', postNew);
+router.get('/connect', getConnect);
+router.get('/disconnect', getDisconnect);
+router.get('/users/me', getMe);
+router.post('/files', postUpload);
+router.get('/files/:id', getShow);
+router.get('/files', getIndex);
+router.put('/files/:id/publish', putPublish);
+router.put('/files/:id/unpublish', putUnpublish);
+router.get('/files/:id/data', getFile);
+
+export default router;
